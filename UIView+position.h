f84e40b73a8a1402898e4cbc2680af520c4b3fa7
Dmitry Ivanov <ethercrow@gmail.com>
2011-03-17 22:10:39 +0600

    initial commit

---
+++ UIView+position.h
@@ -0,0 +1,27 @@
+//
+//  UIView+position.h
+//
+//  Apache license
+//
+//  Created by Tyler Neylon on 3/19/10 (http://bynomial.com/blog/?p=24)
+//  Copyleft 2010 Bynomial.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIView (position)
+
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGSize size;
+
+@property (nonatomic) CGFloat rx;
+@property (nonatomic) CGFloat ry;
+
+// Setting these modifies the origin but not the size.
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat bottom;
+
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+
+@end
---
+++ UIView+position.m
@@ -0,0 +1,86 @@
+//
+//  UIView+position.m
+//
+//  Apache license
+//
+//  Created by Tyler Neylon on 3/19/10 (http://bynomial.com/blog/?p=24)
+//  Copyleft 2010 Bynomial.
+//
+
+#import "UIView+position.h"
+
+
+@implementation UIView (position)
+
+- (CGPoint)origin {
+  return self.frame.origin;
+}
+
+- (void)setOrigin:(CGPoint)newOrigin {
+  self.frame = CGRectMake(newOrigin.x, newOrigin.y, self.frame.size.width, self.frame.size.height);
+}
+
+- (CGSize)size {
+  return self.frame.size;
+}
+
+- (void)setSize:(CGSize)newSize {
+  self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y,
+                          newSize.width, newSize.height);
+}
+
+- (CGFloat)rx {
+  return self.frame.origin.x;
+}
+
+- (void)setRx:(CGFloat)newX {
+  self.frame = CGRectMake(newX, self.frame.origin.y,
+                          self.frame.size.width, self.frame.size.height);
+}
+
+- (CGFloat)ry {
+  return self.frame.origin.y;
+}
+
+- (void)setRy:(CGFloat)newY {
+  self.frame = CGRectMake(self.frame.origin.x, newY,
+                          self.frame.size.width, self.frame.size.height);
+}
+
+- (CGFloat)right {
+  return self.frame.origin.x + self.frame.size.width;
+}
+
+- (void)setRight:(CGFloat)newRight {
+  self.frame = CGRectMake(newRight - self.frame.size.width, self.frame.origin.y,
+                          self.frame.size.width, self.frame.size.height);
+}
+
+- (CGFloat)bottom {
+  return self.frame.origin.y + self.frame.size.height;
+}
+
+- (void)setBottom:(CGFloat)newBottom {
+  self.frame = CGRectMake(self.frame.origin.x, newBottom - self.frame.size.height,
+                          self.frame.size.width, self.frame.size.height);
+}
+
+- (CGFloat)width {
+  return self.frame.size.width;
+}
+
+- (void)setWidth:(CGFloat)newWidth {
+  self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y,
+                          newWidth, self.frame.size.height);
+}
+
+- (CGFloat)frameHeight {
+  return self.frame.size.height;
+}
+
+- (void)setHeight:(CGFloat)newHeight {
+  self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y,
+                          self.frame.size.width, newHeight);
+}
+
+@end
